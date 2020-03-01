8f30539def3d1d826e33f293e945fe8129eed0b5
steekam <waynewanyee@gmail.com>
2020-03-01 22:57:49 +0300

    refactor: changing gh-pages to use docs folder for deployments

--- config.production.php
+++ config.production.php
@@ -3,6 +3,9 @@
 return [
     'baseUrl' => 'https://docs.slashdotlabsprojects.com',
     'production' => true,
+    'build' => [
+      'destination' => 'docs'
+    ],
 
     // DocSearch credentials
     'docsearchApiKey' => '',
